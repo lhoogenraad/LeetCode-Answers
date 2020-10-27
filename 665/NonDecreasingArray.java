4e9f32f527def98815f881f3eb76797c38587142
Leon <le.o.n@outlook.com>
2020-10-27 14:20:22 +1300

    I'm dumb, you can't directly print out an array in java, you need to use a loop to print it out >:(

--- 665/NonDecreasingArray.java
+++ 665/NonDecreasingArray.java
@@ -1,16 +1,43 @@
+// Solution to leetcode question #665 non-decreasing array
+
 public class NonDecreasingArray{
-    /*
-      Given an array nums with n integers, your task is to check if it could 
-      become non-decreasing by modifying at most 1 element.
-      We define an array is non-decreasing if nums[i] <= nums[i + 1]
+    /**
+       Problem Description:
+       Given an array nums with n integers, your task is to check if it could 
+       become non-decreasing by modifying at most 1 element.
+       We define an array is non-decreasing if nums[i] <= nums[i + 1]
       holds for every i (0-based) such that (0 <= i <= n - 2).
-     */
+    */
 
     public static void main(String[] args){
 	int[] arr = {1, 2, 4, 5, 7, 5};
-	System.out.println(checkDecreasing(arr));
+	if(isDecreasing(arr)){
+	    System.out.println("Array is already non-decreasing");
+	}
+	System.out.println(slice(arr, 1, 4));
     }
 
+    /**
+     * If this method is reached by main(), then we can
+     * assume that the array is already not non-decreasing. However
+     * in an actual production environment we'd want there to be checks
+     * to see if the array is already non-decreasing or not.
+     *
+     * Could change the return to return the position of the value that
+     * needs to be changed.
+     */
+    public static boolean checkIfModifiable(int[] array){
+	// The position of the out of place value we found.
+	int pos = -1;
+	
+	for(int i = 0; i < array.length-1; i++){
+	    if(array[i] > array[i+1]){
+		// Check if remaining array is non-decreasing
+		int[] remainingArray = slice(array, i+1, array.length);
+	    }
+	}
+	return false;
+    }
 
     /**
      * This method checks that the given array
@@ -18,12 +45,33 @@ public class NonDecreasingArray{
      * than the value at position i+1)
      * This method takes O(n) time, and O(n) space
      */
-    public static boolean checkDecreasing(int[] array){
+    public static boolean isDecreasing(int[] array){
 	for(int i = 0; i < array.length-1; i++){
 	    if(array[i] > array[i+1]){
 		return false;
 	    }
 	}
 	return true;
+    }
+
+    /**
+     * Returns a subarray of the given array parameter,
+     * indexed by the start and end parameters.
+     */
+    public static int[] slice(int[] array, int startIndex, int endIndex){
+	// Base case checks because I'm a good boy
+	if(endIndex < startIndex){
+	    return null;
+	}else if(endIndex > array.length){
+	    return null;
+	}
+
+	// Actual method logic starts here
+	int[] slice = new int[endIndex-startIndex];
+
+	for(int i = startIndex; i < endIndex; i++){
+	    slice[i-startIndex] = array[i];
+	}
+	return slice;
     }
 }
