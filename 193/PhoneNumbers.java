d88237184bf45951dadb33718d3dc271e68c0a87
Leon <le.o.n@outlook.com>
2020-12-13 15:50:50 +1300

    Bracket inputs seem to work now, will have to do more testing on it of course, but at the moment works on the input.txt file inputs for brackets

--- 193/PhoneNumbers.java
+++ 193/PhoneNumbers.java
@@ -21,7 +21,62 @@ public class PhoneNumbers{
 	String input = "";
 	while(sc.hasNextLine()){
 	    input = sc.nextLine();
-	    System.out.println(input);
+	    if(isValid(input)){
+		System.out.println(input);
+	    }
 	}
+    }
+
+    private static boolean isValid(String phNumber){
+	if(phNumber.charAt(0) == '('){
+	    return validateWithBrackets(phNumber);
+	}else{
+	    //return validateWithoutBrackets(phNumber);
+	}
+	return false;
+    }
+
+    private static boolean validateWithBrackets(String number){
+	if(number.length() != 14){
+	    return false;
+	}
+	String[] split = number.split(" ");
+	// If more than one space present, 
+	if(split.length > 2){
+	    return false;
+	}
+	String brackets = split[0];
+	String numbers = split[1];
+	if(brackets.length() != 5){
+	    return false;
+	}
+	// If final char is not closing bracket, return false
+	if(brackets.charAt(4) != ')'){
+	    return false;
+	}
+	for(int i = 1; i < brackets.length()-1; i++){
+	    // If iterated char is not a number, return false
+	    if(!Character.isDigit(brackets.charAt(i))){
+		return false;
+	    }
+	}
+    
+
+	if(numbers.length() != 8){
+	    return false;
+	}
+
+	String[] numSplit = numbers.split("-");
+	if(numSplit.length != 2){
+	    return false;
+	}
+	for(String s : numSplit){
+	    for(int i = 0; i < s.length(); i++){
+		if(!Character.isDigit(s.charAt(i))){
+		    return false;
+		}
+	    }
+	}
+	return true;
     }
 }
