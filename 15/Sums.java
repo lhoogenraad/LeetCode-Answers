6accbedccfdef9b2f74b7286fd1684e8316fecea
Leon <le.o.n@outlook.com>
2020-12-13 17:19:29 +1300

    Starting work on question 15, 3sum (who named this question dude??)

---
+++ 15/Sums.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+/**
+ * A solution to leetcode question 15.
+ */
+public class Sums{
+    private static Scanner sc = new Scanner(System.in);
+    
+    public static void main(String[] args){
+        int[] array = {-1, 2, 1, -3, 0, 0};
+    }
+}
