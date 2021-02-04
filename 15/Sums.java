7dcff15029f1346a4448a38a29cc1a8c33a7df2b
Leon <le.o.n@outlook.com>
2021-02-04 16:32:47 +1300

    Solution found and implemented

--- 15/Sums.java
+++ 15/Sums.java
@@ -1,5 +1,4 @@
-import java.util.Scanner;
-
+import java.util.*;
 /**
  * A solution to leetcode question 15.
  */
@@ -7,6 +6,56 @@ public class Sums{
     private static Scanner sc = new Scanner(System.in);
     
     public static void main(String[] args){
-        int[] array = {-1, 2, 1, -3, 0, 0};
+	ArrayList<ArrayList<Integer>> triplets = new ArrayList<>();
+        int[] array = {-1, 0, 1, 2, -1, 4};
+	
+	int arrayLen = array.length;
+
+	/*
+	  This is the code that will iterate through the array and check each
+	  combination 
+	 */
+	for(int i = 0; i < arrayLen; i++){
+	    // If i reaches this value, we have gone through all the possible combinations //
+	    if(i > arrayLen-2){
+		break;
+	    }
+	    int iNum = array[i];
+	    for(int x = i+1; x < arrayLen; x++){
+		int xNum = array[x];
+		for(int y = i+2; y < arrayLen; y++){
+		    int yNum = array[y];
+		    if(iNum + xNum + yNum == 0){
+			ArrayList<Integer> newList = new ArrayList<>();
+			newList.add(iNum);
+			newList.add(xNum);
+			newList.add(yNum);
+			triplets.add(newList);
+		    }
+		}
+	    }
+	}
+
+	for(int i = 0; i < triplets.size(); i++){
+	    Collections.sort(triplets.get(i));
+	}
+
+	ArrayList<ArrayList<Integer>> removedDups = new ArrayList<>();
+	// iterate through triplets array
+	for(int i = 0; i < triplets.size(); i++){
+	    // If the set of triplets isn't in removedDups arraylist, add it
+	    // This prevents duplicates being added in future iterations
+	    if(!removedDups.contains(triplets.get(i))){
+		removedDups.add(triplets.get(i));
+	    }
+	}
+
+        for(int i = 0; i < removedDups.size(); i++){
+	    ArrayList<Integer> idk = removedDups.get(i);
+	    for(Integer val : idk){
+		System.out.print(val + " ");
+	    }
+	    System.out.println();
+	}
     }
 }
